9d3cdfd43de61cfcbaa27856f9020b95eeca60fe
Keith Gudger <kgudger@gmail.com>
2026-04-03 13:39:01 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/camera_ohm/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.camera_ohm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CamerOhmApp());
+}
+
+class CamerOhmApp extends StatelessWidget {
+  const CamerOhmApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CamerOhm',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'CamerOhm Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const Text('Welcome to CamerOhm!'),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    appState.toggleScreen(),
+                  },
+                  child: const Text('Toggle Screen'),
+                ),
+                SizedBox(width: 20),
+                Text: 'Select Resistor Values:',             
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
